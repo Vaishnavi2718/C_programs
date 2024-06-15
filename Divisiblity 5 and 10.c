2fddd9077c61e77ba7c59c0e12d5d9778f497279
Vaishnavi2718 <vaishu18906@gmail.com>
2024-06-15 18:23:27 +0530

    Add files via upload

---
+++ Divisiblity 5 and 10.c
@@ -0,0 +1,28 @@
+Program 3: Divisbility check 5 and 10
+include <stdio.h>
+
+
+void main() 
+
+{ 
+   int num;
+   
+printf("\n Enter any number:");
+  
+ scanf("%d",&num);
+   
+if ((num%5==0) &&(num%10==0))
+   
+{
+       printf("Number is divisible by 5 and 10");
+  
+ }
+  
+ else 
+  
+ {printf("Not divisible ");
+   
+}
+  
+ 
+}
