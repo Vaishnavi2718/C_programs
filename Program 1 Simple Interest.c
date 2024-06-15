87f96a339d4667fdf8f5ff48da446fc33c788532
Vaishnavi2718 <vaishu18906@gmail.com>
2024-06-15 18:16:15 +0530

    Add files via upload

---
+++ Program 1 Simple Interest.c
@@ -0,0 +1,29 @@
+Program 1:Simple Interest
+
+#include <stdio.h>
+
+void main()
+
+{
+    
+int p,t;
+    
+float r,si;
+    
+printf("\n Enter the principle");
+    
+scanf("%d", &p);
+    
+printf("\n Enter the rate of interest");
+    
+scanf("%f", &r);
+    
+printf("\n Enter the time");
+    
+scanf("%d",&t);
+    
+si=p*r*t/100;
+    
+printf("si:%f",si);
+
+}
---
+++ Program 2 Area of a triangle.c
@@ -0,0 +1,24 @@
+Program 2 : Area of a triangle
+#include <stdio.h>
+
+void main()
+
+{
+    
+int b,h
+float area;
+
+    
+printf("\n Enter the base of a triangle");
+    
+scanf("%d", &b);
+    
+printf("\n Enter the height of a triangle");
+    
+scanf("%d", &h);
+    
+area=0.5 * b * h;
+    
+printf("area:%f",area);
+
+}
